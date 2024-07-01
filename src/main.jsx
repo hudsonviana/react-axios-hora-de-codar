a7d18d4f378c571ed1aa1d7df3cfa0013d8b7328
hudsonviana <hudson1206@gmail.com>
2024-06-30 23:07:41 -0300

    Home page e axios config

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+
+// Páginas
+import Home from './routes/Home.jsx';
+import NewPost from './routes/NewPost.jsx';
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/new', element: <NewPost /> },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
